68408afb08351af8f995260a085539f0f3c271a2
Yeheum Choi <yh.choi@purple-nest.com>
2022-05-19 17:43:49 +0900

    [ENH] : documents 작업

--- src/components/Layout/Documents/MainContainer/index.tsx
+++ src/components/Layout/Documents/MainContainer/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useResize, useDebounce } from '@/hooks';
 import { mixins } from '@/styles';
 
 const STDContainer = styled.section`
@@ -61,39 +62,73 @@ const STDImageWrapper = styled.div`
   }
 `;
 
+const STDVideoWrapper = styled.div`
+  position: absolute;
+  left: 433px;
+  bottom: 0px;
+  width: 100%;
+
+  img {
+    width: 100%;
+  }
+`;
+
 const DocumentsMainContainer: React.FC = () => {
+  const [selectedItem, setSelectedItem] = useState('temp_image1.jpeg');
   const DATA = [
     {
       id: 'Infinitas',
+      video: 'temp_image1.jpeg',
       description: `“Infinitas(∞)”는 옷을 형상화하는 골조로 ‘선’에 접근한 스테이지이다. 평면에 그려지는 선과 선이 교차하며 형성된 공간은 개인의 개성을 드러내는 이미지로 형상화된다. 원단 위에 그려지는 무한한 선의 상상을 통해 새로운 이미지를 사유하면서 사유의 접점에서 패션의 가능성을 발견하고자 하였다. \n\n "Infinitas(∞)" is a stage that approaches the 'line' with a frame that embodies clothes. The space formed by crossing the line drawn on the plane is shaped as an image that reveals an individual's personality. Through the imagination of infinite lines drawn on the fabric, we tried to discover the possibility of fashion at the interface of thought while thinking about a new image.`,
     },
     {
       id: 'Spectrum',
+      video: 'temp_image2.jpeg',
       description: `고유의 파장을 지닌 채 중첩되어 있던 디자이너들의 사유는 각기 다른 좌표를 추구하며 팔방으로 내닫았다. 원점에서 뻗어 나간 사유는 자신들의 준위에 따라 산재하고 비로소 타자에게 모습을 보인다. 펼쳐진 상상들이 관측되는 순간에 그들은 측정되고, 의미를 부여받고, 존재가 인정된다. “좌표원점”으로 모였던 디자이너들은 각자의방향으로 빛을 쏘았다. 빛들은 저마다의 색으로 흔적을 남겼고, 지금 그 색깔을 공유하고자 한다.\n\nThe thoughts of the designers, which were overlapping with their own wavelengths, went to all directions in pursuit of different coordinates. Thoughts extending from the origin are scattered according to their level, and only then appear to others. The moment unfolded imaginations are observed, they are measured, given meaning, and their existence recognized. The designers who gathered at the “coordinate origin” shot light in their respective directions. The lights left their traces with their own colors, and now we want to share those colors.`,
     },
     {
       id: 'Teleport to',
+      video: 'temp_image3.jpeg',
       description: `전체를 결정짓는 작은 디테일에 주목한 "Teleport to [  ]"는 디자이너 각자의 세계관을 상징하는 이미지들을 디테일한 터치로 시각화했다. 상상력을 자극하는 입체적인 이미지를 패션이라는 매체에 구현함으로써, 디자이너가 의도한 세계로 순간 이동하는 체험을 제공하고자 한다.\n\nThe stage "Teleport to [  ]" focused on the details which controls the whole. Each designer visualized their outlook on the world in symbolic and imaginative images with detailed touches. By representing these images in fashion, this stage intends to provide an experience to 'teleport to' the world which designers want to be.`,
     },
     {
       id: 'Textureacity',
+      video: 'temp_image4.jpeg',
       description: `“Textureacity”는 소재의 가능성을 보여줌과 동시에 다양한 존재들의 낯설지만 아름다운 조화를 탐구한 스테이지이다. 소재를 바탕으로 8명의 디자이너가 도달하고자 하는 지향점은 다르지만, 서로의 차이를 인정하고 이를 표현하는 과정에서 모든 디자이너들의 개성은 하나의 새로운 아름다움으로 수렴되었다. 각자 다른 소재의 결합으로부터 오는 조화와 대비는 패션의 실루엣부터 분위기까지 변화시키면서 의상에서의 무한한 잠재력을 보여주고 있다.\n\n“Textureacity” is a stage that shows the potential of fabrics and explores the unfamiliar but beautiful harmony of various beings. The eight designers' goals are different based on each fabric. In the process of acknowledging and expressing their differences, however, all designers' personalities converged into one new beauty. Harmony and contrast from the combination of different fabrics change from the silhouette to the mood of style, showing the infinite potential in fashion.`,
     },
   ];
+  const windowWidth = useResize().width;
+  const debounceImage = useDebounce(selectedItem, 400);
 
   return (
     <STDContainer>
       <STDContentContainer>
-        {DATA.map(({ id, description }) => (
-          <STDContentWrapper key={id}>
+        {DATA.map(({ id, video, description }) => (
+          <STDContentWrapper
+            key={id}
+            onMouseEnter={() => setSelectedItem(video)}
+          >
             <STDGradientBox />
             <h3 className="h3_style">{id}</h3>
-            <p className="text_style">{description}</p>
+            <p className="text_style">
+              {description}
+              <br />
+              <br />
+              <br />
+            </p>
           </STDContentWrapper>
         ))}
       </STDContentContainer>
       <STDImageWrapper>
         <img className="image_style" src="/image/main_logo.png" />
+        <STDVideoWrapper
+          style={{
+            left: `${(windowWidth - 72) * 0.245}px`,
+            width: `${(windowWidth - 72) * 0.15}px`,
+          }}
+        >
+          <img src={`image/documents/${debounceImage}`} />
+        </STDVideoWrapper>
       </STDImageWrapper>
     </STDContainer>
   );
--- src/components/Layout/Home/HomeContainer.tsx
+++ src/components/Layout/Home/HomeContainer.tsx
@@ -1,350 +1,352 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import { useDebounce } from "@/hooks";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useDebounce } from '@/hooks';
 
 const HomeContainerLayout = styled.div``;
 
 const ThumbnailListLayout = styled.div`
-    padding: 24px;
-    margin-bottom: 478px;
+  padding: 24px;
+  margin-bottom: 478px;
 `;
 
 const TeamLayout = styled.div`
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
 `;
 
 const ThumbnailItem = styled.div`
-    width: 208px;
-    height: 117px;
-    border: 1px solid #fff;
-    cursor: pointer;
+  width: 208px;
+  height: 117px;
+  border: 1px solid #fff;
+  cursor: pointer;
 `;
 
 const BottomLogoLayout = styled.div`
-    position: fixed;
-    bottom: 0px;
-    padding: 0 24px 20px;
+  position: fixed;
+  bottom: 0px;
+  padding: 0 24px 20px;
 `;
 
 const Logo = styled.img`
-    width: 100%;
-    height: auto;
+  width: 100%;
+  height: auto;
 `;
 
 const RowColumnLocationLayout = styled.div`
-    position: absolute;
-    display: flex;
-    top: 0px;
-    /* max-width: 261px; */
-    /* left: 0px; */
+  position: absolute;
+  display: flex;
+  top: 0px;
+  /* max-width: 261px; */
+  /* left: 0px; */
 `;
 
 const LogoLayout = styled.div`
-    position: relative;
+  position: relative;
 `;
 
 const RowNumberImage = styled.img`
-    width: 100%;
+  width: 100%;
 `;
 
 const ColumnNumberImage = styled.img`
-    width: 100%;
+  width: 100%;
 `;
 
 const CommaImage = styled.img``;
 
 const TestLayout = styled.div`
-    display: flex;
-    width: 102px;
+  display: flex;
+  width: 102px;
 `;
 
 const HomeContainer: React.FC = () => {
-    const [row, setRow] = useState("");
-    const [column, setCoumn] = useState("");
-    const debouncedRow = useDebounce(row, 300);
-    const debouncedColumn = useDebounce(column, 300);
+  const [row, setRow] = useState('');
+  const [column, setCoumn] = useState('');
+  const debouncedRow = useDebounce(row, 300);
+  const debouncedColumn = useDebounce(column, 300);
 
-    return (
-        <HomeContainerLayout>
-            <ThumbnailListLayout>
-                {THUMBNAIL_DATA.map((t) => {
-                    return (
-                        <TeamLayout key={t.team}>
-                            {t.thumbnailList.map((item) => {
-                                return (
-                                    <ThumbnailItem
-                                        onMouseOver={() => {
-                                            setRow(item.row);
-                                            setCoumn(item.column);
-                                        }}></ThumbnailItem>
-                                );
-                            })}
-                        </TeamLayout>
-                    );
-                })}
-                {/* <img src={"/image/f_1.svg"} /> */}
-                {/* {THUMBNAIL_DATA.map((t) => {
+  return (
+    <HomeContainerLayout>
+      <ThumbnailListLayout>
+        {THUMBNAIL_DATA.map((t) => {
+          return (
+            <TeamLayout key={t.team}>
+              {t.thumbnailList.map((item) => {
+                return (
+                  <ThumbnailItem
+                    key={`${item.row},${item.column}`}
+                    onMouseOver={() => {
+                      setRow(item.row);
+                      setCoumn(item.column);
+                    }}
+                  ></ThumbnailItem>
+                );
+              })}
+            </TeamLayout>
+          );
+        })}
+        {/* <img src={"/image/f_1.svg"} /> */}
+        {/* {THUMBNAIL_DATA.map((t) => {
           return;
         })} */}
-            </ThumbnailListLayout>
-            <BottomLogoLayout>
-                <LogoLayout>
-                    <Logo src={"/image/main_logo.png"} />
-                    <RowColumnLocationLayout>
-                        <TestLayout>
-                            <RowNumberImage src={debouncedRow} />
-                            <CommaImage src={"/image/comma.svg"} />
-                            <ColumnNumberImage src={debouncedColumn} />
-                        </TestLayout>
-                    </RowColumnLocationLayout>
-                </LogoLayout>
-            </BottomLogoLayout>
-        </HomeContainerLayout>
-    );
+      </ThumbnailListLayout>
+      <BottomLogoLayout>
+        <LogoLayout>
+          <Logo src={'/image/main_logo.png'} />
+          <RowColumnLocationLayout>
+            <TestLayout>
+              <RowNumberImage src={debouncedRow} />
+              <CommaImage src={'/image/comma.svg'} />
+              <ColumnNumberImage src={debouncedColumn} />
+            </TestLayout>
+          </RowColumnLocationLayout>
+        </LogoLayout>
+      </BottomLogoLayout>
+    </HomeContainerLayout>
+  );
 };
 
 export default HomeContainer;
 
 const THUMBNAIL_DATA: {
-    team: string;
-    thumbnailList: {
-        image: string;
-        row: string;
-        column: string;
-        koreanName: string;
-        englishName: string;
-    }[];
+  team: string;
+  thumbnailList: {
+    image: string;
+    row: string;
+    column: string;
+    koreanName: string;
+    englishName: string;
+  }[];
 }[] = [
-    {
-        team: "Infinitas",
-        thumbnailList: [
-            {
-                image: "",
-                row: "/image/f_1.svg",
-                column: "/image/b_1.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_1.svg",
-                column: "/image/b_2.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_1.svg",
-                column: "/image/b_3.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_1.svg",
-                column: "/image/b_4.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_1.svg",
-                column: "/image/b_5.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_1.svg",
-                column: "/image/b_6.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_1.svg",
-                column: "/image/b_7.svg",
-                koreanName: "",
-                englishName: "",
-            },
-        ],
-    },
-    {
-        team: "Spectrum",
-        thumbnailList: [
-            {
-                image: "",
-                row: "/image/f_2.svg",
-                column: "/image/b_1.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_2.svg",
-                column: "/image/b_2.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_2.svg",
-                column: "/image/b_3.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_2.svg",
-                column: "/image/b_4.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_2.svg",
-                column: "/image/b_5.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_2.svg",
-                column: "/image/b_6.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_2.svg",
-                column: "/image/b_7.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_2.svg",
-                column: "/image/b_8.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_2.svg",
-                column: "/image/b_9.svg",
-                koreanName: "",
-                englishName: "",
-            },
-        ],
-    },
-    {
-        team: "Teleport to",
-        thumbnailList: [
-            {
-                image: "",
-                row: "/image/f_3.svg",
-                column: "/image/b_1.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_3.svg",
-                column: "/image/b_2.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_3.svg",
-                column: "/image/b_3.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_3.svg",
-                column: "/image/b_4.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_3.svg",
-                column: "/image/b_5.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_3.svg",
-                column: "/image/b_6.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_3.svg",
-                column: "/image/b_7.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_3.svg",
-                column: "/image/b_8.svg",
-                koreanName: "",
-                englishName: "",
-            },
-        ],
-    },
-    {
-        team: "Textureacit",
-        thumbnailList: [
-            {
-                image: "",
-                row: "/image/f_4.svg",
-                column: "/image/b_1.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_4.svg",
-                column: "/image/b_2.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_4.svg",
-                column: "/image/b_3.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_4.svg",
-                column: "/image/b_4.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_4.svg",
-                column: "/image/b_5.svg",
-                koreanName: "",
-                englishName: "",
-            },
-            {
-                image: "",
-                row: "/image/f_4.svg",
-                column: "/image/b_6.svg",
-                koreanName: "",
-                englishName: "",
-            },
-        ],
-    },
+  {
+    team: 'Infinitas',
+    thumbnailList: [
+      {
+        image: '',
+        row: '/image/f_1.svg',
+        column: '/image/b_1.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_1.svg',
+        column: '/image/b_2.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_1.svg',
+        column: '/image/b_3.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_1.svg',
+        column: '/image/b_4.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_1.svg',
+        column: '/image/b_5.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_1.svg',
+        column: '/image/b_6.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_1.svg',
+        column: '/image/b_7.svg',
+        koreanName: '',
+        englishName: '',
+      },
+    ],
+  },
+  {
+    team: 'Spectrum',
+    thumbnailList: [
+      {
+        image: '',
+        row: '/image/f_2.svg',
+        column: '/image/b_1.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_2.svg',
+        column: '/image/b_2.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_2.svg',
+        column: '/image/b_3.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_2.svg',
+        column: '/image/b_4.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_2.svg',
+        column: '/image/b_5.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_2.svg',
+        column: '/image/b_6.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_2.svg',
+        column: '/image/b_7.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_2.svg',
+        column: '/image/b_8.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_2.svg',
+        column: '/image/b_9.svg',
+        koreanName: '',
+        englishName: '',
+      },
+    ],
+  },
+  {
+    team: 'Teleport to',
+    thumbnailList: [
+      {
+        image: '',
+        row: '/image/f_3.svg',
+        column: '/image/b_1.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_3.svg',
+        column: '/image/b_2.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_3.svg',
+        column: '/image/b_3.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_3.svg',
+        column: '/image/b_4.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_3.svg',
+        column: '/image/b_5.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_3.svg',
+        column: '/image/b_6.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_3.svg',
+        column: '/image/b_7.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_3.svg',
+        column: '/image/b_8.svg',
+        koreanName: '',
+        englishName: '',
+      },
+    ],
+  },
+  {
+    team: 'Textureacit',
+    thumbnailList: [
+      {
+        image: '',
+        row: '/image/f_4.svg',
+        column: '/image/b_1.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_4.svg',
+        column: '/image/b_2.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_4.svg',
+        column: '/image/b_3.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_4.svg',
+        column: '/image/b_4.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_4.svg',
+        column: '/image/b_5.svg',
+        koreanName: '',
+        englishName: '',
+      },
+      {
+        image: '',
+        row: '/image/f_4.svg',
+        column: '/image/b_6.svg',
+        koreanName: '',
+        englishName: '',
+      },
+    ],
+  },
 ];
