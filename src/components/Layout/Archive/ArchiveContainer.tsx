1092546c0239f41ed1fb2a4e38b06e3776751e88
Gunoo-Kim <gunoo@cherground.com>
2022-05-23 22:10:54 +0900

    add

---
+++ src/components/Layout/Archive/ArchiveContainer.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState, useRef } from "react";
+import styled from "styled-components";
+
+const STDContainer = styled.div``;
+
+const ArchiveContainer: React.VFC = () => {
+    return <STDContainer></STDContainer>;
+};
+
+export default ArchiveContainer;
---
+++ src/components/Layout/Archive/components/DesignerList.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const STDContainer = styled.div``;
+
+const DesignerList: React.VFC = () => {
+    return <STDContainer></STDContainer>;
+};
+
+export default DesignerList;
