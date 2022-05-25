49d0533f0ec931322b989105041715a8daadc901
Gunoo-Kim <gunoo@cherground.com>
2022-05-25 12:28:59 +0900

    add) video

--- src/pages/archive/index.tsx
+++ src/pages/archive/index.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react";
 import { NextPage } from "next";
 import Router from "next/router";
+import { Archive } from "@/components";
 
 const ArchivePage: NextPage = () => {
-    return <div>archive page</div>;
+    return <Archive.MaincContainer />;
 };
 
 export default ArchivePage;
