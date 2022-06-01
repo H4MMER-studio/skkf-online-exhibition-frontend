538aff118718dc93ad834797047040eb7988e2c2
Gunoo-Kim <gunoo@cherground.com>
2022-06-01 15:31:29 +0900

    test

--- src/pages/archive/index.tsx
+++ src/pages/archive/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NextPage } from 'next';
 
 const ThanksPage: NextPage = () => {
-  return <div>먼데</div>;
+  return <div>먼</div>;
 };
 
 export default ThanksPage;
